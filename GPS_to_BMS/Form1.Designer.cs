66fff76c94a339beea2107b703ada494c226e481
Kevin Lourenço <kevin.lourenco@hotmail.com>
2019-12-04 12:27:41 +0000

    First Commit

---
+++ GPS_to_BMS/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace GPS_to_BMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(485, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 406);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\Users\\kevin\\Desktop\\ficheiro.kml";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(476, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(50, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "192.168.1.73";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(339, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Adress and Port of Smartphone connected: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 377);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(291, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select KML to update GPS coordinates:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(48, 164);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(592, 197);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Debug Output:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(342, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(110, 26);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "50000";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(318, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = ":";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.Location = new System.Drawing.Point(44, 561);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(596, 76);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "START";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(50, 406);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(21, 20);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(50, 497);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(21, 20);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Enabled = false;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "COM10",
+            "COM11",
+            "COM12",
+            "COM13",
+            "COM14",
+            "COM15",
+            "COM16",
+            "COM17",
+            "COM18",
+            "COM19",
+            "COM20",
+            "COM21",
+            "COM22",
+            "COM23",
+            "COM24",
+            "COM25",
+            "COM26",
+            "COM27",
+            "COM28",
+            "COM29",
+            "COM30"});
+            this.listBox1.Location = new System.Drawing.Point(96, 496);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 24);
+            this.listBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 467);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(291, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Select KML to update GPS coordinates:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 670);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(714, 726);
+            this.MinimumSize = new System.Drawing.Size(714, 726);
+            this.Name = "Form1";
+            this.Text = "GPS to BMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button button2;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GPS_to_BMS/Form1.cs
@@ -0,0 +1,328 @@
+﻿using NmeaParser.Nmea;
+using NmeaParser.Nmea.Gps;
+using SharpKml.Base;
+using SharpKml.Dom;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using Point = SharpKml.Dom.Point;
+
+namespace GPS_to_BMS
+{
+    public partial class Form1 : Form
+    {
+        public delegate void delUpdateUITextBox(String text, Color color);
+        NetworkStream ns;
+        StreamWriter writer;
+        StreamReader reader;
+        TcpClient svrSkt;
+        bool TcpConnected = false;
+        bool IsRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void UpdateUITextBox(string text, Color color)
+        {
+            string date = DateTime.Now + " - ";
+            richTextBox1.AppendText(date);
+            richTextBox1.Select(richTextBox1.Text.Length - date.Length, date.Length);
+            richTextBox1.SelectionColor = Color.DarkBlue;
+            richTextBox1.DeselectAll();
+            richTextBox1.AppendText(text);
+            richTextBox1.Select(richTextBox1.Text.Length - text.Length, text.Length);
+            richTextBox1.SelectionColor = color;
+            richTextBox1.DeselectAll();
+        }
+
+
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                svrSkt = new TcpClient(textBox2.Text, int.Parse(textBox3.Text));
+                richTextBox1.Invoke(new delUpdateUITextBox(this.UpdateUITextBox), new object[] { "Connection accepted!\n ", Color.Green });
+                ns = svrSkt.GetStream();
+                reader = new StreamReader(ns);
+                TcpConnected = true;
+                
+                string nmea = reader.ReadLine();
+                richTextBox1.Invoke(new delUpdateUITextBox(this.UpdateUITextBox), new object[] { nmea+"\n ", Color.Green });
+                
+            }
+            catch (Exception Ex)
+            {
+                richTextBox1.Invoke(new delUpdateUITextBox(this.UpdateUITextBox), new object[] { "Connection refused!\nError: "+Ex.Message, Color.Red });
+            }
+            
+
+        }
+
+
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.  
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Filter = "KML file|*.kml",
+                Title = "Select a KML File to be Updated",
+                InitialDirectory = AppDomain.CurrentDomain.BaseDirectory
+            };
+            try
+            {
+                // Show the Dialog.  
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string dbDir = openFileDialog1.FileName;
+                    textBox1.Text = dbDir;
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+        }
+
+
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (!IsRunning)
+                {
+                    button3.Text = "STOP";
+                    try
+                    {
+                        printText("Initializing!\n ", Color.Green); 
+                        Thread.Sleep(500);
+                        svrSkt = new TcpClient(textBox2.Text, int.Parse(textBox3.Text));
+                        printText("Connection accepted!\n ", Color.Green);
+                        Thread.Sleep(500);
+                        ns = svrSkt.GetStream();
+                        reader = new StreamReader(ns);
+                        TcpConnected = true;
+
+                        string nmea = reader.ReadLine();
+                        printText(nmea + "\n ", Color.Green);
+
+                    }
+                    catch (Exception Ex)
+                    {
+                        printText("Connection refused!\n Error: " + Ex.Message,Color.Red);
+                        if (svrSkt!=null)
+                        {
+                            svrSkt.Close();
+                        }
+                        button3.Text = "START";
+                    }
+                    ThreadTCPlistener thread = new ThreadTCPlistener(this, svrSkt, reader, 4000, textBox1.Text);
+                    Task.Run(() =>
+                    {
+                        // While asynchronously waiting for this to complete, 
+                        // the thread is given back to the thread-pool
+                        thread.updateGPScoordinates();
+                    });
+                   
+                    
+                }
+                else {
+                    button3.Text = "START";
+                    printText("Stopping...\n ", Color.Green);
+                    svrSkt.Close();
+                    printText("Fully stopped\n ", Color.Green);
+                }
+            }
+            else
+            {
+                string name;
+                string message;
+                StringComparer stringComparer = StringComparer.OrdinalIgnoreCase;
+                Thread readThread = new Thread(PortChat.Read);
+                SerialPort _serialPort;
+
+                // Create a new SerialPort object with default settings.
+                _serialPort = new SerialPort();
+
+                // Allow the user to set the appropriate properties.
+                _serialPort.PortName = PortChat.SetPortName("COM3");
+                _serialPort.BaudRate = PortChat.SetPortBaudRate(9600);
+                _serialPort.Parity = PortChat.SetPortParity(_serialPort.Parity);
+                _serialPort.DataBits = PortChat.SetPortDataBits(_serialPort.DataBits);
+                _serialPort.StopBits = PortChat.SetPortStopBits(_serialPort.StopBits);
+                _serialPort.Handshake = PortChat.SetPortHandshake(_serialPort.Handshake);
+
+                // Set the read/write timeouts
+                _serialPort.ReadTimeout = 500;
+                _serialPort.WriteTimeout = 500;
+
+                _serialPort.Open();
+                bool _continue = true;
+                readThread.Start();
+
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Type QUIT to exit");
+
+                while (_continue)
+                {
+                    message = Console.ReadLine();
+
+                    if (stringComparer.Equals("quit", message))
+                    {
+                        _continue = false;
+                    }
+                    else
+                    {
+                        _serialPort.WriteLine(
+                            String.Format("<{0}>: {1}", name, message));
+                    }
+                }
+                readThread.Join();
+                _serialPort.Close();
+            }
+
+           
+        }
+
+
+        internal void printText(string texto, Color color)
+        {
+            richTextBox1.Invoke(new delUpdateUITextBox(this.UpdateUITextBox), new object[] { texto, color });
+            
+        }
+
+
+        internal void writeToKmlFile(string filePath, string lat, string lon, string alt)
+        {
+            try
+            {
+                string longitude = lon.Replace(",", ".");
+                string longitudeInt = longitude.Substring(0, longitude.IndexOf(".") + 1);
+                string longitudeDecimal = longitude.TrimStart(longitudeInt.ToArray()).Substring(0, 6);
+                longitude = longitudeInt + longitudeDecimal;
+
+                string latitude = lat.Replace(",", ".");
+                string latitudeInt = latitude.Substring(0, latitude.IndexOf(".") + 1);
+                string latitudeDecimal = latitude.TrimStart(latitudeInt.ToArray()).Substring(0, 6);
+                latitude = latitudeInt + latitudeDecimal;
+ 
+
+                //Here is the variable with which you assign a new value to the attribute
+                string stringFile = System.Text.Encoding.UTF8.GetString(Properties.Resources.kmlFile);
+                string finalString = stringFile.Replace("LONG", longitude);
+                finalString = finalString.Replace("LAT", latitude);
+                Console.WriteLine(finalString);
+                System.IO.File.WriteAllText(filePath, finalString);
+                
+            }
+            catch (Exception Ex)
+            {
+                throw;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                radioButton1.Checked = false;
+                button2.Enabled = false;
+                textBox1.Enabled = false;
+                listBox1.Enabled = true;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                radioButton2.Checked = false;
+                button2.Enabled = true;
+                textBox1.Enabled = true;
+                listBox1.Enabled = false;
+            }
+           
+
+        }
+    }
+
+
+
+
+
+    public class ThreadTCPlistener
+    {
+        Form1 appForm;
+        StreamReader reader;
+        int period;
+        TcpClient svrSkt;
+        string filePath;
+
+        public ThreadTCPlistener(Form1 form1, TcpClient svrSkt, StreamReader reader, int periodInMilli,string filePath)
+        {
+            this.reader = reader;
+            this.period = periodInMilli;
+            this.appForm = form1;
+            this.svrSkt = svrSkt;
+            this.filePath = filePath;
+        }
+
+
+        public void updateGPScoordinates()
+        {
+            try
+            {
+                NetworkStream ns = svrSkt.GetStream();
+                reader = new StreamReader(ns);
+
+                string texto="";
+            
+                while (true)
+                {
+                    string nmeaString = reader.ReadLine();
+                    //string nmeaString = "$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47"; //debugging purposes!
+                    if (nmeaString.Contains("$GPGGA") && !nmeaString.Split(',')[2].Equals(""))
+                    {
+                        Gga nmeaGpgga = (Gga)NmeaParser.Nmea.Gps.Gpgga.Parse(nmeaString);
+
+                        texto = nmeaGpgga.ToString();
+                        appForm.printText(texto,Color.Black);
+                        Console.WriteLine(texto);
+
+                        appForm.writeToKmlFile(filePath, nmeaGpgga.Latitude.ToString(),nmeaGpgga.Longitude.ToString(), nmeaGpgga.Altitude.ToString());
+                        Thread.Sleep(period);
+                    }
+                }
+
+            }
+            
+            catch (Exception Ex)
+            {
+                appForm.printText(Ex.ToString(), Color.Black);            }
+
+
+            
+        }
+
+    }
+
+}
